c87f982fd337b870dfc7ad0719b5c9e60e5d23d5
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 12:29:26 +0330

    p6

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,13 +1,25 @@
 function List() {
-    const fruits = [{ id:1, name: 'apple', calories: 95 },
-    { id:2, name: 'orange', calories: 45 },
-    { id:3, name: 'banana', calories: 105 },
-    { id:4, name: 'coconut', calories: 156 },
-    { id:5, name: 'pineapple', calories: 37 }];
+    const fruits = [{ id: 1, name: 'apple', calories: 95 },
+    { id: 2, name: 'orange', calories: 45 },
+    { id: 3, name: 'banana', calories: 105 },
+    { id: 4, name: 'coconut', calories: 156 },
+    { id: 5, name: 'pineapple', calories: 37 }];
 
 
-// we should have unique keys , so we define ids
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name} </li>);
+    // fruits.sort((a,b)=> a.name.localeCompare(b.name)); //alphabetical order
+    // fruits.sort((a,b)=> b.name.localeCompare(a.name)); // reverse alphabetical order
+
+    // fruits.sort((a,b) => a.calories-b.calories);//sort by calories (Numerical order)
+    // fruits.sort((a, b) => b.calories - a.calories);//sort by calories (Reverse Numerical order)
+
+    
+
+
+
+
+    // we should have unique keys , so we define ids
+    const listItems = fruits.map(fruit => <li key={fruit.id}>
+        {fruit.name} : &nbsp; <b>{fruit.calories} </b> </li>);
 
     return (
         <ol>{listItems}</ol>
