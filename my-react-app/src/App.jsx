10799ec17a7bebd0d3e459ae2e9976591d061145
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 09:42:22 +0330

    p4

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,23 @@
-import Button from './Button'
+// props = read-only properties that are shared between components. 
+// A parent component can send data to a child component.
+// < component key=value / > 
+
+//propTypes = a mechanism that ensures that the passed value is of the correct datatype.
+// age: PropTypes.number 
+
+
+
+import Student from './Student'
 
 function App() {
 
   return (
     <>
+<Student name="Spongebob" age={30} isStudent={true}/>
+<Student name="Patrick" age={42} isStudent={false}/>
+<Student name="Squidward" age={42} isStudent={false}/>
+<Student name="Sandy" age={27} isStudent={true}/>
 
-      <Button></Button>
     </>
   );
 }
