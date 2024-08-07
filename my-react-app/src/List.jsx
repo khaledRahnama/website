c20f47080dff6eb424acc7424afb34fc4506bfa8
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 14:10:23 +0330

    p6

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -13,14 +13,15 @@ function List() {
     // fruits.sort((a, b) => b.calories - a.calories);//sort by calories (Reverse Numerical order)
 
 
-    const lowCalFruit = fruits.filter(fruit => fruit.calories < 100);
+    // const lowCalFruit = fruits.filter(fruit => fruit.calories < 100);
+    const highCalFruit = fruits.filter(fruit => fruit.calories >= 100);
 
 
 
     // we should have unique keys , so we define ids
-    const listItems = lowCalFruit.map(lowCalFruit => <li key={lowCalFruit.id}>
-        {lowCalFruit.name} : &nbsp;
-        <b>{lowCalFruit.calories} </b> </li>);
+    const listItems = highCalFruit.map(highCalFruit => <li key={highCalFruit.id}>
+        {highCalFruit.name} : &nbsp;
+        <b>{highCalFruit.calories} </b> </li>);
 
     return (
         <ol>{listItems}</ol>
