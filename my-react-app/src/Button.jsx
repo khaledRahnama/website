a92fde7a8e0b2eafd841dd855293198579f219b5
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 17:52:19 +0330

    p7EventHandler

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,12 +1,17 @@
 function Button() {
 
-    const handleClick = () => console.log("OUCH!")
+    const handleClick = () => console.log("OUCH!");
+
+    const handleClick2 = (name) => console.log(`${name} stop clicking me`)
 
     return( 
         
+        // <button className="button" onClick={handleClick}> Click me 😀 </button>
 
-        <button className="button" onClick={handleClick}> Click me 😀 </button>
-        
+        // <button className="button" onClick={handleClick2}> Click me 😀 </button>
+        <button className="button" onClick={()=> handleClick2("Khaled")}> Click me 😀 </button>
+
+
     );
 
 }
