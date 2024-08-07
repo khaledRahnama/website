572ec9066d97412d88a1276d10d3a44404cbe601
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 09:13:42 +0330

    p3

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,7 +1,25 @@
-import styles from './Button/Button.module.css'
 function Button() {
+    //inline css styling , it prevents global style conflicts becasue we are not dealing with  classNames 
+    // it is easy to understand and it is convenient 
+    // it is great for isolated components with minimum styling
+
+    // different styling 
+    // 1. External 
+    // 2. Modules 
+    // 3. Inline
+
+    const styles = {
+
+        backgroundColor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+
+    }
     return (
-        <button className={styles.button}>Click Me</button>
+        <button style={styles}>Click Me</button>
     );
 
 }
