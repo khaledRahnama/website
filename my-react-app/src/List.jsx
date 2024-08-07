df382b3af63b0515a25f63eb882ea7d0dda34bf7
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 12:19:19 +0330

    p6

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,9 +1,15 @@
-function List()
-{
-    const fruits = [ 'apple', 'orange', 'banana', 'coconut', 'pineapple'];
-    const listItems = fruits.map(fruit => <li>{fruit}</li> );
+function List() {
+    const fruits = [{ name: 'apple', calories: 95 },
+    { name: 'orange', calories: 45 },
+    { name: 'banana', calories: 105 },
+    { name: 'coconut', calories: 156 },
+    { name: 'pineapple', calories: 37 }];
 
-    return(
+
+// we should have unique keys , so we define ids
+    const listItems = fruits.map(fruit => <li key={fruit.name}>{fruit.name} </li>);
+
+    return (
         <ol>{listItems}</ol>
     );
 
