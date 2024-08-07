e1902d7317a3f870c22bcd00c05c31db80f12f81
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 07:10:16 +0330

    p1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import Food from "./Food"
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+    <Food></Food>
+    <Footer></Footer>
+    </>
+  
+  )
+}
+
+export default App
