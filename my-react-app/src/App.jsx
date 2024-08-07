30d3418e10e278718f64bd5e2170e27594aa13f6
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 11:11:50 +0330

    p6

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,10 @@
-//Conditional rendering = allows you to control what gets rendered 
-// in your application based on certain conditions 
-// (show , hide , or change components)
-import UserGreeting from "./UserGreeting";
+import List from "./List";
+
 function App() {
 
   return (
     <>
-      <UserGreeting isLoggedIn={true} username="khaled" />
-      <hr />
-      <UserGreeting isLoggedIn={false} username="AnotherUser" />
-      <hr />
-      <UserGreeting isLoggedIn={true} />
-      <hr />
-      <UserGreeting></UserGreeting>
+<List></List>
     </>
   );
 }
---
+++ my-react-app/src/List.jsx
@@ -0,0 +1,8 @@
+function List()
+{
+    const fruits = [ 'apple', 'orange', 'banana', 'coconut', 'pineapple'];
+
+    return(fruits)
+
+}
+export default List
