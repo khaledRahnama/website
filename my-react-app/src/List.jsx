8a0d264e16f58634f486391be6aca1171bd6895e
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 12:12:07 +0330

    p6

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,8 +1,11 @@
 function List()
 {
     const fruits = [ 'apple', 'orange', 'banana', 'coconut', 'pineapple'];
+    const listItems = fruits.map(fruit => <li>{fruit}</li> );
 
-    return(fruits)
+    return(
+        <ul>{listItems}</ul>
+    );
 
 }
 export default List
