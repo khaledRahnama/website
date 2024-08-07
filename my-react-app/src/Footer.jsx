e9d57edded6c9a7543bb85ebdbc6831c6b7aafeb
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-08-07 07:14:39 +0330

    p1

--- my-react-app/src/Footer.jsx
+++ my-react-app/src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
      return(
         <footer> 
-            <p> &copy; { new Date().getFullYear} website name </p>
+            <p> &copy; { new Date().getFullYear()} website name </p>
         </footer>
      );
 }
